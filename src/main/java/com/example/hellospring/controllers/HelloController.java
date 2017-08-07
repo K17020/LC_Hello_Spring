1b19ed98f152db7889508c62d3a1f8d78067a41e
Kevin Tam <tam.kevin@hotmail.com>
2017-08-07 16:53:56 -0700

    Setup basic route in spring framework

--- src/main/java/com/example/hellospring/controllers/HelloController.java
+++ src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -1,26 +1,56 @@
 package com.example.hellospring.controllers;
 
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.PublicKey;
 
 @Controller
 public class HelloController {
 
-    // This is the route for the webpage
+    // This handles the root of the web app
     @RequestMapping(value = "")
     @ResponseBody
-    // Returns text Hello World at the root path
-    public String index(){
-        return "Hello World";
+    public String index(HttpServletRequest request){
+        String name = request.getParameter("name");
+
+        // if the query is a null then have is display "Hello World
+        if (name == null){
+            name = "World";
+        }
+        return "Hello " + name;
     }
-    @RequestMapping(value = "goodbye")
+
+    // Creates an HTML form and handles get request methods
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
+    public String helloForm(){
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+        return html;
+    }
+
+    // This method handles post from the form method above
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request){
+        String name = request.getParameter("name");
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name){
+        return "Hello " + name;
+    }
+
+    // redirects to the root of the web app if this route is called
+    @RequestMapping(value = "goodbye")
     public String goodbye(){
-        return "Goodbye";
+        return "redirect:/";
     }
 }
